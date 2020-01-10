00b1cf4d0c08218f8598594f78f3784262de3cfa
sathaphorn.stp <sathaphorn.stp@gmail.com>
2020-01-10 22:11:22 +0700

    add logger

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '@env/environment';
+import { Logger } from 'src/app/core';
+
+const log = new Logger('App');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+
+  ngOnInit(): void {
+    // Setup logger
+    if (environment.production) {
+      Logger.enableProductionMode();
+    }
+
+    log.debug('init');
+  }
+
+
 }
