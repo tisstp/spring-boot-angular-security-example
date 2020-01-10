00f7965a690d9cfc0dca21d6546eef8f0efd8c59
sathaphorn.stp <sathaphorn.stp@gmail.com>
2020-01-10 23:46:21 +0700

    allow request method option

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # spring-boot-angular-security-example
 Security Demo with Spring boot and Angular
+
+
+## Run Application
+```bash
+# Angular
+$ cd angular 
+$ yarn install
+$ yarn run start
+
+
+# spring boot
+$ gradle bootRun
+$ ./gradlew tasks --stacktrace
+```
+
+
+## CLI
+```bash
+# spring cli
+$ sdk install springboot
+$ spring --version
+$ spring help run
+
+# angular cli
+
+
+# gradle cli
+```
---
+++ springboot/src/main/java/com/tisstp/example/securitydemo/config/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.tisstp.example.securitydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author sathaphorn.stp (Tis)
+ * @since 10-01-2020, 11:24 PM
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .headers().disable()
+      .csrf().disable()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and().authorizeRequests()
+      .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+      .anyRequest().authenticated();
+
+    // disable page caching
+    http.headers().cacheControl();
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("OPTIONS");
+    config.addAllowedMethod("GET");
+    config.addAllowedMethod("POST");
+    config.addAllowedMethod("PUT");
+    config.addAllowedMethod("PATCH");
+    config.addAllowedMethod("DELETE");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+
+}
