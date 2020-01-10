b4024741cdaafd741734ca46afce3dc74c8e074f
sathaphorn.stp <sathaphorn.stp@gmail.com>
2020-01-10 22:40:55 +0700

    set display screen

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from '@env/environment';
 import { Logger } from 'src/app/core';
+import { AuthenticationService } from 'src/app/core/authentication/authentication.service';
+import { ExampleService } from 'src/app/services/example.service';
 
 const log = new Logger('App');
 
@@ -12,6 +14,12 @@ const log = new Logger('App');
 export class AppComponent implements OnInit {
   title = 'angular';
 
+  constructor(
+    private authService: AuthenticationService,
+    private exampleService: ExampleService
+  ) {
+  }
+
   ngOnInit(): void {
     // Setup logger
     if (environment.production) {
@@ -21,5 +29,18 @@ export class AppComponent implements OnInit {
     log.debug('init');
   }
 
+  onWithoutCsrf() {
+    log.debug('onWithoutCsrf');
+    this.authService.withCsrfIgnore({}).subscribe((val) => {
+      log.debug('res:', val);
+    });
+  }
+
+  onWithCsrf() {
+    log.debug('onWithCsrf');
+    this.exampleService.withCsrfXsrf({}).subscribe((val) => {
+      log.debug('res:', val);
+    });
+  }
 
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ApiPrefixInterceptor } from 'src/app/core/http/api-prefix.interceptor';
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ApiPrefixInterceptor, multi: true }
