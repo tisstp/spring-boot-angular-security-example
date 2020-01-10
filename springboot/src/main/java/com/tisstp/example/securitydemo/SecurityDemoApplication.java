6f4d6b4539dd50573417c88f265175de44ee49e8
sathaphorn.stp <sathaphorn.stp@gmail.com>
2020-01-10 23:23:07 +0700

    set spring boot web server

--- springboot/src/main/java/com/tisstp/example/securitydemo/SecurityDemoApplication.java
+++ springboot/src/main/java/com/tisstp/example/securitydemo/SecurityDemoApplication.java
@@ -1,13 +1,26 @@
 package com.tisstp.example.securitydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
-public class SecurityDemoApplication {
+public class SecurityDemoApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityDemoApplication.class, args);
-	}
+  @Autowired
+  private Environment env;
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecurityDemoApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println(String.format("Server port(property): %s", env.getProperty("server.port")));
+  }
 
 }
