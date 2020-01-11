f740b091c99dfef7bf0a7e4258f427986d75cb03
sathaphorn.stp <sathaphorn.stp@gmail.com>
2020-01-11 14:45:25 +0700

    move cors config & add dev properties

---
+++ springboot/src/main/java/com/tisstp/example/securitydemo/config/CorsConfig.java
@@ -0,0 +1,65 @@
+package com.tisstp.example.securitydemo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * @author sathaphorn.stp (Tis)
+ * @since 11-01-2020, 1:51 PM
+ */
+@Log4j2
+@Configuration
+public class CorsConfig {
+
+  @Value("${management.endpoints.web.cors.allowed-origins}")
+  private String[] allowOrigins;
+
+  /**
+   * Global CORS configuration : Access-Control-Allow-Origin: https://amazing.site read docs:
+   * https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSMissingAllowOrigin
+   *
+   * @return WebMvcConfigurer
+   */
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    log.debug("CORS Configurer: start");
+    log.debug(String.format("CORS Allow Origin: %s", Arrays.toString(allowOrigins)));
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+          .addMapping("/**")
+          .allowedOrigins(allowOrigins);
+      }
+    };
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    log.debug("CORS Filter: start");
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("OPTIONS");
+    config.addAllowedMethod("GET");
+    config.addAllowedMethod("POST");
+    config.addAllowedMethod("PUT");
+    config.addAllowedMethod("PATCH");
+    config.addAllowedMethod("DELETE");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+
+}
--- springboot/src/main/java/com/tisstp/example/securitydemo/config/WebSecurityConfig.java
+++ springboot/src/main/java/com/tisstp/example/securitydemo/config/WebSecurityConfig.java
@@ -1,8 +1,5 @@
 package com.tisstp.example.securitydemo.config;
 
-import java.util.Arrays;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,11 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.tisstp.example.securitydemo.security.CustomCsrfFilter;
 import lombok.extern.log4j.Log4j2;
@@ -51,44 +43,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     // disable page caching
     http.headers().cacheControl().disable();
-  }
-
-  /**
-   * Global CORS configuration : Access-Control-Allow-Origin: https://amazing.site read docs:
-   * https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/Errors/CORSMissingAllowOrigin
-   *
-   * @return WebMvcConfigurer
-   */
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    log.debug("CORS Configurer: start");
-//    log.debug(String.format("CORS Allow Origin: %s", Arrays.toString(this.allowOrigins)));
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry
-          .addMapping("/**")
-          .allowedOrigins("http://localhost:4200");
-      }
-    };
-  }
-
-  @Bean
-  public CorsFilter corsFilter() {
-    log.debug("CORS Filter: start");
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    config.addAllowedOrigin("*");
-    config.addAllowedHeader("*");
-    config.addAllowedMethod("OPTIONS");
-    config.addAllowedMethod("GET");
-    config.addAllowedMethod("POST");
-    config.addAllowedMethod("PUT");
-    config.addAllowedMethod("PATCH");
-    config.addAllowedMethod("DELETE");
-    source.registerCorsConfiguration("/**", config);
-    return new CorsFilter(source);
   }
 
   private CsrfTokenRepository csrfTokenRepository() {
