e71ebf4340f4c2b6e9668e3522f0d265be292d45
sathaphorn.stp <sathaphorn.stp@gmail.com>
2020-02-16 14:42:03 +0700

    rename example to home controller

--- springboot/src/main/java/com/tisstp/example/securitydemo/controller/HomeController.java
+++ springboot/src/main/java/com/tisstp/example/securitydemo/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,8 @@ import lombok.extern.log4j.Log4j2;
  */
 @Log4j2
 @RestController
-public class ExampleController {
+@RequestMapping("home")
+public class HomeController {
 
   @PostMapping("/example")
   @ResponseBody
