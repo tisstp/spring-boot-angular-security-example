f45e2ef08b8659a6d59784e09fac38d16cd91bce
sathaphorn.stp <sathaphorn.stp@gmail.com>
2020-02-29 18:38:58 +0700

    import BrowserAnimationsModule

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,15 +6,18 @@ import { CoreModule } from 'src/app/core';
 import { AppComponent } from 'src/app/core/containers';
 import { RootStoreModule } from 'src/app/store';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
-    RootStoreModule,
     CoreModule,
-    extModules // In a production build you would want to disable the Store Devtools
+    RootStoreModule,
+    // In a production build you would want to disable the Store Devtools
+    extModules
   ],
   providers: [],
   bootstrap: [AppComponent]
